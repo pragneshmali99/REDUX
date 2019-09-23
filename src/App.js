27bc5642d5fd16c2dfbf5668ff0069991a4d1cba
Akash Gohel <akash.gohel.37231@gmail.com>
2019-09-23 12:52:02 +0530

    Fundamental of Redux

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Link } from "@reach/router";
+import { Provider } from 'react-redux';
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+import store from "./store";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </Provider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/SearchParams.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect, useContext } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import { connect } from 'react-redux';
+import useDropdown from "./useDropdown";
+import Results from "./Results";
+import changeTheme from "./actionCreators/changeTheme";  
+import changeLocation from "./actionCreators/changeLocation";
+
+const SearchParams = props => {
+  const [breeds, updateBreeds] = useState([]);
+  const [pets, setPets] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location: props.location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals || []);
+  }
+
+  useEffect(() => {
+    updateBreeds([]);
+    updateBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      updateBreeds(breedStrings);
+    }, console.error);
+  }, [animal]);
+
+  return (
+    <div className="search-params">
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={props.location}
+            placeholder="Location"
+            onChange={e => props.updateLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <label htmlFor="location">
+          Theme
+          <select
+            value={props.theme}
+            onChange={e => props.setTheme(e.target.value)}
+            onBlur={e => props.setTheme(e.target.value)}
+          >
+            <option value="peru">Peru</option>
+            <option value="darkblue">Dark Blue</option>
+            <option value="chartreuse">Chartreuse</option>
+            <option value="mediumorchid">Medium Orchid</option>
+          </select>
+        </label>
+        <button style={{ backgroundColor: props.theme }}>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
+};
+
+
+const mapStateToProps = ({ theme, location }) => ({
+  theme,
+  location
+});
+
+const mapDispatchToProps = dispatch => ({
+  setTheme: theme => dispatch(changeTheme(theme)),
+  updateLocation: location => dispatch(changeLocation(location))
+});
+
+export default connect(mapStateToProps,mapDispatchToProps)(SearchParams);
